e242e749cf5b6ffe29d10ec1594b086790790be7
sryabova <ganyim@inbox.ru>
2021-05-28 00:24:44 +0500

    ade code

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,14 @@
+import java.net.SocketOption;
+
 public class Main {
+    public static void main(String[] args) {
+        int priceInPenny;
+        int mile;
+        int conditionForMileInPenny;
+
+        priceInPenny = 1000000_20;
+        conditionForMileInPenny = 2000;
+        mile = (int) (priceInPenny / conditionForMileInPenny);
+        System.out.println("Количество миль: " + mile);
+    }
 }
